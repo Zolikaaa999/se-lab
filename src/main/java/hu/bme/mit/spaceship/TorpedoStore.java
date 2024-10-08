f770e59c10d4950bd742d30f766353f98972ded8
Terék Zoltán Tamás <zoltan.t.terek@gmail.com>
2024-10-08 20:03:11 +0200

    revie_changes

--- src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
+++ src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
@@ -12,9 +12,9 @@ public class TorpedoStore {
   // rate of failing to fire torpedos [0.0, 1.0]
   private double FAILURE_RATE = 0.0; //NOSONAR
 
-  private int torpedoCount = 0;
+  private int torpedoCount = 0; 
 
-  private Random generator = new Random();
+  private Random generator = new Random(); 
 
   public TorpedoStore(int numberOfTorpedos){
     this.torpedoCount = numberOfTorpedos;
@@ -32,7 +32,7 @@ public class TorpedoStore {
 
   public boolean fire(int numberOfTorpedos){
     if(numberOfTorpedos < 1 || numberOfTorpedos > this.torpedoCount){
-      throw new IllegalArgumentException("numberOfTorpedos");
+      throw new IllegalArgumentException("numberOfTorpedos"); // needed to throw
     }
 
     boolean success = false;
@@ -43,7 +43,7 @@ public class TorpedoStore {
 
     if (r >= FAILURE_RATE) {
       // successful firing
-      this.torpedoCount -= numberOfTorpedos;
+      this.torpedoCount -= numberOfTorpedos; // "-=" instead of ""=-" is better
       success = true;
     } else {
       // simulated failure
